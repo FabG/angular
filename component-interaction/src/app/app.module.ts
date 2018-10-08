fcf2a0a3dea41d5b931d1cdae5e3242df0906f93
FabG <fabrice.guillaume@gmail.com>
2018-10-08 12:53:31 -0400

    Pipes for View formating

--- component-interaction/src/app/app.module.ts
+++ component-interaction/src/app/app.module.ts
@@ -3,11 +3,13 @@ import { NgModule } from '@angular/core';
 
 import { AppComponent } from './app.component';
 import { TestComponent } from './test/test.component';
+import { PipesComponent } from './pipes/pipes.component';
 
 @NgModule({
   declarations: [
     AppComponent,
-    TestComponent
+    TestComponent,
+    PipesComponent
   ],
   imports: [
     BrowserModule
